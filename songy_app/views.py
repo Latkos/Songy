8dd252c932db73fcdfe8067edf803a0d0fd53373
Latkos <48084189+Latkos@users.noreply.github.com>
2021-05-16 22:07:11 +0200

    Passing a whole list to game template
    It's not a final version, it lacks formatting, lacks JS. But POST and forwarding list work.

--- songy_app/views.py
+++ songy_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 # Create your views here.
 from .spotify_handler import SongListCreator
-
+from .spotify_handler import Song
 
 # def songify(request):
 #     song_list_creator = SongListCreator()
@@ -12,12 +12,10 @@ from .spotify_handler import SongListCreator
 #     return HttpResponse('display.html')
 
 def songy(request):
-
     return render(request, 'songy.html', {})
 
 
 def faq(request):
-
     return render(request, 'faq.html', {})
 
 
@@ -30,4 +28,12 @@ def contact(request):
 
 
 def game(request):
-    return render(request, 'game.html', {})
+    if request.method=='POST':
+        print("HURA\n\n\n")
+        print(request.POST)
+        song_list_creator=SongListCreator()
+        song_list=song_list_creator.create_song_list(['https://open.spotify.com/playlist/37i9dQZF1DWXhcuQw7KIeM'])
+        print(type(song_list))
+        for song in song_list:
+            song.display()
+    return render(request, 'game.html', {"song_list":song_list})
