f97dae1cf1c3ad32ef6d2a98f0397abaf1760f5f
Latkos <48084189+Latkos@users.noreply.github.com>
2021-05-19 20:12:00 +0200

    Semi-final version in terms of JS functions
    What remains is beautifying, cleanup, edge cases etc.

--- songy_app/views.py
+++ songy_app/views.py
@@ -5,12 +5,6 @@ from .spotify_handler import SongListCreator
 from .spotify_handler import Song
 import json
 
-# def songify(request):
-#     song_list_creator = SongListCreator()
-#     song_list = song_list_creator.create_song_list(['https://open.spotify.com/playlist/2ArQdshMTWzfI8OZQiP7tj?si=db9b5024318b4418'])
-#     for song in song_list:
-#         song.display()
-#     return HttpResponse('display.html')
 
 def songy(request):
     return render(request, 'songy.html', {})
@@ -29,11 +23,13 @@ def contact(request):
 
 
 def game(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         print(request.POST)
-        time=request.POST['time']
-        link=request.POST['spotify_link']
-        song_list_creator=SongListCreator()
-        song_list=song_list_creator.create_song_list_passable_to_JS([link])
-        return render(request, 'game.html', {'titles_list':json.dumps(song_list[0]),'artists_list':json.dumps(song_list[1]),'time':time,'link':link})
-
+        time = request.POST['time']
+        link = request.POST['spotify_link']
+        round_length = request.POST['round_length']
+        song_list_creator = SongListCreator()
+        song_list = song_list_creator.create_song_list_passable_to_JS([link])
+        return render(request, 'game.html',
+                      {'titles_list': json.dumps(song_list[0]), 'artists_list': json.dumps(song_list[1]), 'time': time,
+                       'link': link, 'round_length': round_length})
